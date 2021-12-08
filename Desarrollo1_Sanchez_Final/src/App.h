5f4dfff1317fe31efb7062c79b13d734743d0f06
pancho404 <franciscosanchez2101@gmail.com>
2021-12-08 18:05:29 -0300

    Created app class.

---
+++ Desarrollo1_Sanchez_Final/src/App.h
@@ -0,0 +1,11 @@
+#pragma once
+#include "SceneManager.h"
+#include "Menu.h"
+class App
+{
+public:
+	App();
+	~App();
+	void start();
+};
+
---
+++ Desarrollo1_Sanchez_Final/src/main.cpp
@@ -0,0 +1,7 @@
+#include "raylib.h"
+#include "App.h"
+
+void main()
+{
+	InitWindow(1280, 720, "Pool by Francisco Sanchez");
+}
