d00dd01f1cef1ce1e928dd92d7c002e3401a6c8c
pancho404 <franciscosanchez2101@gmail.com>
2021-12-08 18:12:34 -0300

    Added Rules Scene.

---
+++ Desarrollo1_Sanchez_Final/src/Rules.cpp
@@ -0,0 +1,31 @@
+#include "Rules.h"
+
+Rules::Rules(SceneManager* sceneManager)
+{
+	this->sceneManager = sceneManager;
+	backRec.width = 178;
+	backRec.height = 71;
+	backRec.x = 546;
+	backRec.y = 634;
+	texture = LoadTexture("../res/rules.png");
+}
+
+Rules::~Rules()
+{
+	UnloadTexture(texture);
+}
+
+void Rules::Update()
+{
+	if (CheckCollisionPointRec(GetMousePosition(), backRec) && IsMouseButtonPressed(MouseButton::MOUSE_LEFT_BUTTON))
+	{
+		sceneManager->setScene(Scene::MENU);
+	}
+}
+
+void Rules::Draw()
+{
+	BeginDrawing();
+	DrawTexture(texture, 0, 0, WHITE);
+	EndDrawing();
+}
---
+++ Desarrollo1_Sanchez_Final/src/Rules.h
@@ -0,0 +1,16 @@
+#pragma once
+#include "raylib.h"
+#include "SceneManager.h"
+class Rules
+{
+private:
+	Rectangle backRec;
+	SceneManager* sceneManager;
+	Texture2D texture;
+public:
+	Rules(SceneManager* sceneManager);
+	~Rules();
+	void Draw();
+	void Update();
+};
+
